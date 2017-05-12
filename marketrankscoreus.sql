63f4e5c6099ab8f8ebb87bf8aed4ca0628ab0d72
Bill Kitchens <billkitchens@gmail.com>
2017-05-12 12:20:11 -0500

    MarketRanksCoreus
    Market ranks for US over time

---
+++ marketrankscoreus.sql
@@ -0,0 +1,7 @@
+select * from regionperiodregionalcombinedsummary
+where regiontype = 'COREUS NONYC'
+and regioncode = 'US PRIMARYSECONDARY'
+and period between '200703' and '201703'
+and groupingclass = 'MarketRank'
+
+
