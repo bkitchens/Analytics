444a222440e2682b80d21107132433a7d147317a
Bill Kitchens <billkitchens@gmail.com>
2017-05-26 14:18:31 -0500

    Add files via upload
    Construction of apartment properties over time, grouped by metro

--- marketrankscoreus.sql
+++ marketrankscoreus.sql
@@ -1,7 +1,10 @@
-select * from regionperiodregionalcombinedsummary
-where regiontype = 'COREUS NONYC'
-and regioncode = 'US PRIMARYSECONDARY'
-and period between '200703' and '201703'
-and groupingclass = 'MarketRank'
+select '201003' as period, msa, m.marketname, count(id) as properties, sum(totalunits) as totalunits from tblapt a
+join market m on a.msa = m.marketid
+where makeperiod(constructionstart) <= '201003'
+and makeperiod(constructionfinish) > '201003'
+and (makeperiod(deletedate) > makeperiod(constructionfinish) OR deletedate is null)
+group by period, msa, m.marketname 
+
+
 
 
