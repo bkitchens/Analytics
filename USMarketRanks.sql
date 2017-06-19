e494e71105d54846d5d95a1764fa7e09bad564b2
Bill Kitchens <billkitchens@gmail.com>
2017-06-19 10:45:07 -0500

    Delete USMarketRanks.sql

--- USMarketRanks.sql
+++
@@ -1,5 +0,0 @@
-select * from regionperiodregionalcombinedsummary
-where regiontype = 'COREUS NONYC'
-and regioncode = 'US PRIMARYSECONDARY'
-and period between '200703' and '201703'
-and groupingclass = 'MarketRank'
